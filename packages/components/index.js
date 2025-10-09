2661092a9bf9b62cf9e2078bd7480aeda4e555d0
guhl <guhongliang@hltn.com>
2025-10-09 18:03:42 +0800

    feat: catalog

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,2 +1,10 @@
 import { format } from '@packages/utils'
+import { flatten} from 'lodash-es'
+import { ref } from "vue"
+
+const count = ref(0)
+console.log(count)
 console.log(format(new Date()))
+
+const arr = [[1,2,3],[4,5,6],[7,8,9,10]]
+console.log(flatten(arr))
