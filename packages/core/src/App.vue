b09ac74a1bab8c49364ff653ac7e2964a6a85030
guhl <895215054@qq.com>
2025-10-31 01:09:06 +0800

    feat: 添加响应式数据监听示例代码
    在App.vue中添加Vue响应式数据(data)和watch监听器的示例代码，展示两种不同的监听方式

--- packages/core/src/App.vue
+++ packages/core/src/App.vue
@@ -4,4 +4,25 @@
 
 <script setup>
 import HelloWorld from '@/component/HelloWorld.vue';
+import { reactive, watch } from 'vue'
+let data = reactive({
+  count: 0
+})
+
+watch(() => data, (newVal)=> {
+  console.log('function');
+  console.log("new val", newVal.count)
+}, { deep: true })
+
+watch(data, (newVal)=> {
+  console.log('reactive obj');
+  console.log("new val", newVal.count)
+})
+
+setTimeout(() => {
+  data.count++
+  data = reactive({
+    count: 2
+  })
+}, 1000)
 </script>
