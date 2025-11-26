17c832969d82c4675394d8ebe582c0834be16fb3
guhl <guhongliang@hltn.com>
2025-11-26 19:10:38 +0800

    chore: update

--- packages/core/src/App.vue
+++ packages/core/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <HelloWorld />
+  {{ data }}
 </template>
 
 <script setup>
-import HelloWorld from '@/component/HelloWorld.vue';
 import { reactive, watch } from 'vue'
-let data = reactive({
-  count: 0
-})
+let data = reactive({ count: 0 })
 
 watch(() => data, (newVal)=> {
-  console.log('new val function', newVal.count); // output: 2
+  console.log('new val function', newVal.count)
 }, { deep: true })
 
 watch(data, (newVal)=> {
-  console.log('new val reactive obj', newVal.count); // output: 1
+  console.log('new val reactive obj', newVal.count)
 })
 
 setTimeout(() => {
   data.count++
-  data = reactive({
-    count: 2
-  })
+  data = reactive({ count: 2 })
 }, 1000)
 </script>
